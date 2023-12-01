c9b31f1a611144971ff3a2a4bcb9fafd63627bd3
elnidala <latorrs.daniel1@gmail.com>
2023-12-01 23:02:33 +0100

    code correction and doxygen documentation

--- knn.c
+++ knn.c
@@ -1,13 +1,10 @@
 #include "knn.h"
-#include <stdlib.h>
-#include <math.h>
-#include <stdio.h>
 
-// Implementation of the Minkowski distance function.
+// Implementation of Minkowski distance
 double minkowskiDistance(ShapeData a, ShapeData b, int featureCount, int p) {
-    if (p <= 0) {
-        fprintf(stderr, "Invalid p value for Minkowski distance: %d\n", p);
-        return ERR_INVALID_P_VALUE; // Error code for invalid p value.
+    if (p <= 0 || !a.features || !b.features) {
+        fprintf(stderr, "Invalid parameters for Minkowski distance\n");
+        return KNN_ERR_INVALID_P;
     }
 
     double sum = 0.0;
@@ -17,55 +14,54 @@ double minkowskiDistance(ShapeData a, ShapeData b, int featureCount, int p) {
     return pow(sum, 1.0 / p);
 }
 
-// Precompute distances between test and training samples.
-double** precomputeDistances(ShapeData *trainingSet, int trainingSize, ShapeData *testSet, int testSize, int featureCount, int p) {
-    double **distances = malloc(testSize * sizeof(double *));
-    if (!distances) {
-        perror("Memory allocation failed for distances array");
+// Precompute distances between test and training samples
+double **precomputeDistances(ShapeData *trainingSet, int trainingSize, ShapeData *testSet, int testSize, int featureCount, int p) {
+    if (!trainingSet || !testSet || p <= 0) {
+        fprintf(stderr, "Invalid parameters for precomputing distances\n");
         return NULL;
     }
+
+    double **distances = malloc(testSize * sizeof(double *));
+    if (!distances) return NULL;
 
     for (int i = 0; i < testSize; i++) {
         distances[i] = malloc(trainingSize * sizeof(double));
         if (!distances[i]) {
-            for (int j = 0; j < i; j++) {
-                free(distances[j]);
-            }
+            // Clean up in case of memory allocation failure
+            for (int j = 0; j < i; j++) free(distances[j]);
             free(distances);
-            perror("Memory allocation failed for distances row");
             return NULL;
         }
 
         for (int j = 0; j < trainingSize; j++) {
             distances[i][j] = minkowskiDistance(testSet[i], trainingSet[j], featureCount, p);
-            if (distances[i][j] < 0) {
-                fprintf(stderr, "Error computing distance between samples %d and %d\n", i, j);
-                exit(ERR_DISTANCE);
+            if (distances[i][j] > 0) {
+                // Clean up and exit in case of invalid p value
+                for (int k = 0; k <= i; k++) free(distances[k]);
+                free(distances);
+                return NULL;
             }
         }
     }
     return distances;
 }
 
-// Comparator function for qsort.
+// Comparator for sorting distance-label pairs
 static int compareDistanceLabels(const void *a, const void *b) {
     const DistanceLabel *dlA = (const DistanceLabel *)a;
     const DistanceLabel *dlB = (const DistanceLabel *)b;
     return (dlA->distance > dlB->distance) - (dlA->distance < dlB->distance);
 }
 
-// k-NN classification function.
+// k-NN classification implementation
 int knnClassify(double **distances, int testIndex, ShapeData *trainingSet, int trainingSize, int k) {
-    if (k <= 0 || k > trainingSize) {
-        fprintf(stderr, "Invalid k value for k-NN classification: %d\n", k);
-        return -1; // Error code for invalid k value.
+    if (k <= 0 || k > trainingSize || !distances || !trainingSet) {
+        fprintf(stderr, "Invalid parameters for k-NN classification\n");
+        return KNN_ERR_INVALID_K;
     }
 
     DistanceLabel *distanceLabels = malloc(trainingSize * sizeof(DistanceLabel));
-    if (!distanceLabels) {
-        perror("Memory allocation failed for DistanceLabel array");
-        return -1; // Error code for memory allocation failure.
-    }
+    if (!distanceLabels) return KNN_ERR_MEMORY_ALLOCATION;
 
     for (int i = 0; i < trainingSize; i++) {
         distanceLabels[i].distance = distances[testIndex][i];
@@ -74,17 +70,17 @@ int knnClassify(double **distances, int testIndex, ShapeData *trainingSet, int t
 
     qsort(distanceLabels, trainingSize, sizeof(DistanceLabel), compareDistanceLabels);
 
+    // Determine the most frequent class among the k nearest neighbors
     int predictedClass = -1, maxCount = 0;
     for (int i = 0; i < k; i++) {
+        int currentLabel = distanceLabels[i].label;
         int count = 1;
-        for (int j = i + 1; j < k && j < trainingSize; j++) {
-            if (distanceLabels[j].label == distanceLabels[i].label) {
-                count++;
-            }
+        for (int j = i + 1; j < k && distanceLabels[j].label == currentLabel; j++) {
+            count++;
         }
         if (count > maxCount) {
             maxCount = count;
-            predictedClass = distanceLabels[i].label;
+            predictedClass = currentLabel;
         }
     }
 
--- knn.h
+++ knn.h
@@ -3,47 +3,61 @@
 
 #include "data_reader.h" // Include for ShapeData structure definition.
 
-// Error codes
-#define ERR_INVALID_P_VALUE -1.0
-#define ERR_DISTANCE 2
+#include <math.h>
 
-// DistanceLabel: Structure to associate a distance with a class label.
-// Used in the k-NN algorithm for mapping distances to training sample labels.
+// Error codes
+#define KNN_SUCCESS 0
+#define KNN_ERR_INVALID_P -1
+#define KNN_ERR_INVALID_K -2
+#define KNN_ERR_MEMORY_ALLOCATION -3
+#define KNN_ERR_NULL_POINTER -4
+
+
+/** 
+ * Structure to associate a distance with a class label.
+ * Used in the k-NN algorithm for mapping distances to training sample labels.
+ */
 typedef struct {
     double distance; // Distance between test and training samples.
     int label;       // Class label of the training sample.
 } DistanceLabel;
 
-// DistanceFunction: Pointer type for various distance calculation functions.
+/** 
+ * DistanceFunction: Pointer type for various distance calculation functions.
+ */
 typedef double (*DistanceFunction)(ShapeData, ShapeData, int);
 
-// minkowskiDistance: Calculates Minkowski distance between two ShapeData instances.
-// Parameters:
-//   a, b: ShapeData instances for comparison.
-//   featureCount: Number of features in each ShapeData instance.
-//   p: Minkowski distance exponent (1 for Manhattan, 2 for Euclidean, etc.).
-// Returns: Calculated Minkowski distance.
+/**
+ * Calculates Minkowski distance between two ShapeData instances.
+ * @param a First ShapeData instance.
+ * @param b Second ShapeData instance.
+ * @param featureCount Number of features in each ShapeData instance.
+ * @param p Minkowski distance exponent (1 for Manhattan, 2 for Euclidean, etc.).
+ * @return Calculated Minkowski distance, or KNN_ERR_INVALID_P for invalid p value.
+ */
 double minkowskiDistance(ShapeData a, ShapeData b, int featureCount, int p);
 
-// precomputeDistances: Precomputes distances between test and training samples.
-// Parameters:
-//   trainingSet: Array of ShapeData for training samples.
-//   trainingSize: Number of samples in the training set.
-//   testSet: Array of ShapeData for test samples.
-//   testSize: Number of samples in the test set.
-//   featureCount: Number of features in each ShapeData instance.
-//   p: Minkowski distance exponent.
-// Returns: 2D array of precomputed distances, NULL on memory allocation failure.
+/**
+ * Precomputes distances between test and training samples.
+ * @param trainingSet Array of training samples.
+ * @param trainingSize Number of samples in the training set.
+ * @param testSet Array of test samples.
+ * @param testSize Number of samples in the test set.
+ * @param featureCount Number of features in each ShapeData instance.
+ * @param p Minkowski distance exponent.
+ * @return 2D array of distances, NULL on memory allocation failure.
+ */
 double** precomputeDistances(ShapeData *trainingSet, int trainingSize, ShapeData *testSet, int testSize, int featureCount, int p);
 
-// knnClassify: Classifies a test sample using the k-NN algorithm.
-// Parameters:
-//   distances: 2D array of precomputed distances.
-//   testIndex: Index of the test sample in the test set.
-//   trainingSet: Array of ShapeData for training samples.
-//   trainingSize: Number of samples in the training set.
-//   k: Number of nearest neighbors for classification.
-// Returns: Predicted class label for the test sample, -1 on error.
+/**
+ * Classifies a test sample using the k-NN algorithm.
+ * @param distances Precomputed distances array.
+ * @param testIndex Index of the test sample.
+ * @param trainingSet Array of training samples.
+ * @param trainingSize Number of samples in the training set.
+ * @param k Number of nearest neighbors to use.
+ * @return Predicted class label, or KNN_ERR_INVALID_K for invalid k value.
+ */
 int knnClassify(double **distances, int testIndex, ShapeData *trainingSet, int trainingSize, int k);
 
 #endif // KNN_H
