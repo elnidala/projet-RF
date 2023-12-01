54ad81b630409a044ab56d18eb1ac1878251d1d4
elnidala <latorrs.daniel1@gmail.com>
2023-12-01 15:34:56 +0100

    modularity and doxygen documentation

--- data_reader.c
+++ data_reader.c
@@ -1,178 +1,155 @@
 #include "data_reader.h"
 
-// Reads and processes all files with a given extension in a specified directory.
-// Populates an array of ShapeData structures with their contents.
+// Parses the filename to extract class and sample information.
+int parseFilename(const char *filename, int *class, int *sample) {
+    // Extracting the actual filename from the path
+    const char *filename_ptr = strrchr(filename, '/');
+    filename_ptr = filename_ptr ? filename_ptr + 1 : filename;
+
+    // Using sscanf to extract class and sample information from the filename
+    if (sscanf(filename_ptr, "s%02dn%03d", class, sample) != 2) {
+        return ERR_INVALID_FILENAME; // Return an error if the format is incorrect
+    }
+    return SUCCESS;
+}
+
+// Allocates memory for storing feature values.
+int allocateFeatures(ShapeData *data) {
+    // Allocate memory for the feature values array
+    data->features = malloc(data->featureCount * sizeof(double));
+    if (!data->features) {
+        return ERR_MEMORY_ALLOCATION_FAILED; // Return an error if allocation fails
+    }
+    return SUCCESS;
+}
+
+// Reads feature values from a file and stores them in an array.
+int readFeaturesFromFile(FILE *file, double *features, int featureCount) {
+    char buffer[1024];
+    char *endptr;
+    for (int i = 0; i < featureCount; i++) {
+        // Read a line from the file
+        if (!fgets(buffer, sizeof(buffer), file)) {
+            return ERR_FEATURES_VALUES; // Return an error if reading fails
+        }
+        // Convert the line to a double value and store it in the array
+        features[i] = strtod(buffer, &endptr);
+        if (buffer == endptr) {
+            return ERR_FEATURES_VALUES; // Return an error if conversion fails
+        }
+    }
+    return SUCCESS;
+}
+
+// Reads and processes all files with the specified extension in a directory.
 ShapeData* readAllFiles(const char *directory, const char *extension, int *count) {
-    DIR *dir;                // Directory stream
-    struct dirent *ent;      // For directory entries
-    int capacity = 100;      // Initial capacity for ShapeData array.
-    int n = 0;               // Counter for number of files processed.
-    ShapeData *data = malloc(capacity * sizeof(ShapeData)); // Allocate initial array.
-    
-    // Check for memory allocation failure
+    DIR *dir;
+    struct dirent *ent;
+    int capacity = 100, n = 0;
+    ShapeData *data = malloc(capacity * sizeof(ShapeData));
     if (!data) {
         perror("Memory allocation failed for ShapeData array");
-        exit(ERR_MEMORY_ALLOCATION_FAILED); 
+        exit(ERR_MEMORY_ALLOCATION_FAILED);
     }
 
-    // Attempt to open the directory
     dir = opendir(directory);
     if (!dir) {
         perror("Unable to open directory");
-        free(data); // Clean up allocated memory
+        free(data);
         exit(ERR_DIR_OPEN_FAILED);
     }
 
-    // Read directory entries
     while ((ent = readdir(dir)) != NULL) {
-        // Check for the specified file extension
         if (strstr(ent->d_name, extension)) {
-            // Calculate required buffer size for filename
-            int required_size = snprintf(NULL, 0, "%s/%s", directory, ent->d_name) + 1;
-            char *filename = malloc(required_size);
-
-            // Check for memory allocation failure for filename
+            // Construct the full file path
+            char *filename = malloc(strlen(directory) + strlen(ent->d_name) + 2);
             if (!filename) {
-                fprintf(stderr, "Failed to allocate memory for filename\n");
+                perror("Memory allocation failed for filename");
                 exit(ERR_MEMORY_ALLOCATION_FAILED);
             }
-            
-            // Construct full filepath and check for snprintf errors
-            int written = snprintf(filename, required_size, "%s/%s", directory, ent->d_name);
-            if (written < 0 || written >= required_size) {
-                fprintf(stderr, "Error or truncation occurred in snprintf\n");
-                free(filename); // Free the filename string
-                exit(ERR_MEMORY_ALLOCATION_FAILED);
-            }
-            
-            // Process the file and store its data
+
+            sprintf(filename, "%s/%s", directory, ent->d_name);
             ShapeData fileData = readFile(filename);
-            if (fileData.features != NULL) {
-                // Check if array capacity needs to be increased
-                if (n >= capacity) {
-                    capacity *= 2; // Double the capacity if the initial capacity is exceeded.
-                    ShapeData *temp = realloc(data, capacity * sizeof(ShapeData));
-                    if (!temp) {
-                        perror("Memory allocation failed during resizing");
-                        free(filename); // Free the filename string
-                        freeShapeData(data, n); 
-                        closedir(dir);
-                        exit(ERR_MEMORY_ALLOCATION_FAILED);
-                    }
-                    data = temp;
+            free(filename);
+
+            // Check if the array needs to be resized
+            if (n >= capacity) {
+                capacity *= 2;
+                ShapeData *temp = realloc(data, capacity * sizeof(ShapeData));
+                if (!temp) {
+                    perror("Memory allocation failed during resizing");
+                    freeShapeData(data, n);
+                    closedir(dir);
+                    exit(ERR_MEMORY_ALLOCATION_FAILED);
                 }
-                data[n++] = fileData; // Store the read data
+                data = temp;
             }
-
-            else {
-                    fprintf(stderr, "Failed to process file: %s\n", filename);
-            }
-            free(filename); // Clean up filename after processing
+            data[n++] = fileData; // Add the read data to the array
         }
     }
 
-    closedir(dir); // Close the directory once all files are processed.
-    *count = n; // Set the count of read files.
-    return data; // Return the array of ShapeData.
+    closedir(dir);
+    *count = n; // Update the count of read files
+    return data; // Return the array of ShapeData
 }
 
-// Reads a single file to extract shape data.
+// Reads a single file and extracts shape data.
 ShapeData readFile(const char *filename) {
-    // Attempt to open the file for reading
-    FILE *file = fopen(filename, "r"); // Open file for reading.
-    if (!file) {
-        // Error handling if file opening fails.
-        fprintf(stderr, "Error opening file %s\n", filename);
-        exit(ERR_INVALID_FILENAME);
-    }
-
     ShapeData data;
-    // Initialize ShapeData structure.
-    data.class = 0;
-    data.sample = 0;
-
-    // Determine the expected number of features based on the file name.
-    data.featureCount = getExpectedFeatureCount(filename);
-
-    // Check for invalid feature count
-    if (data.featureCount < 0) {
-        perror("Error getting expected features");
-        fclose(file);
-        exit(ERR_FEATURES_VALUES);
-    }
-    
-
-    // Allocate memory for storing feature values.
-    data.features = malloc(data.featureCount * sizeof(double));
-    if (data.features == NULL) {
-        // Error handling for memory allocation failure.
-        perror("Memory allocation failed for features");
-        fclose(file);
-        exit(ERR_MEMORY_ALLOCATION_FAILED); 
-    }
-
-    // Extract class and sample number from filename.
-    const char *filename_ptr = strrchr(filename, '/');
-    if (!filename_ptr) {
-        filename_ptr = filename; // Handle filenames without a path.
-    } else {
-        filename_ptr++; // Skip the '/' character.
-    }
-
-    // Parse class and sample information from filename.
-    if (sscanf(filename_ptr, "s%02dn%03d", &data.class, &data.sample) != 2) {
+    // Extract class and sample information from the filename
+    if (parseFilename(filename, &data.class, &data.sample) != SUCCESS) {
         fprintf(stderr, "Filename format not recognized: %s\n", filename);
-        fclose(file);
-        free(data.features);
         exit(ERR_INVALID_FILENAME);
     }
 
-    // Read and store each feature value from the file.
-    char buffer[1024];
-    int featureIndex = 0;
-    char *endptr;
-
-    while (featureIndex < data.featureCount && fgets(buffer, sizeof(buffer), file) != NULL) {
-        double featureValue = strtod(buffer, &endptr);
-
-        // Check if conversion succeeded
-        if (buffer == endptr) {
-            fprintf(stderr, "Conversion error in file %s for feature at index %d\n", filename, featureIndex);
-            exit(ERR_FEATURES_VALUES);
-        }
-
-        data.features[featureIndex] = featureValue;
-        featureIndex++;
-    }
-
-    // Verify the expected number of features were read
-    if (featureIndex != data.featureCount) {
-        fprintf(stderr, "Unexpected number of features in file %s: expected %d, got %d\n", filename, data.featureCount, featureIndex);
+    // Determine the expected number of features based on the filename
+    data.featureCount = getExpectedFeatureCount(filename);
+    if (data.featureCount < 0) {
+        fprintf(stderr, "Error getting expected features for file: %s\n", filename);
         exit(ERR_FEATURES_VALUES);
     }
 
-    fclose(file);
-    return data;
+    // Allocate memory for the features
+    if (allocateFeatures(&data) != SUCCESS) {
+        fprintf(stderr, "Memory allocation failed for features of file: %s\n", filename);
+        exit(ERR_MEMORY_ALLOCATION_FAILED);
+    }
 
+    // Open the file for reading
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        fprintf(stderr, "Error opening file %s\n", filename);
+        exit(ERR_FILE_OPEN_FAILED);
+    }
+
+    // Read features from the file
+    if (readFeaturesFromFile(file, data.features, data.featureCount) != SUCCESS) {
+        fprintf(stderr, "Error reading features from file: %s\n", filename);
+        fclose(file);
+        exit(ERR_FEATURES_VALUES);
+    }
+
+    fclose(file); // Close the file
+    return data; // Return the read data
 }
 
-// Frees the dynamically allocated memory for an array of ShapeData.
+// Frees the dynamically allocated memory for an array of ShapeData structures.
 void freeShapeData(ShapeData *data, int count) {
     for (int i = 0; i < count; i++) {
-        free(data[i].features); // Free the features array for each ShapeData.
+        free(data[i].features); // Free the memory for each features array
     }
-    free(data); // Free the array of ShapeData itself.
+    free(data); // Free the memory for the array of ShapeData
 }
 
-
-// Determines the number of expected features based on file extension.
+// Determines the expected number of features based on file extension.
 int getExpectedFeatureCount(const char *filename) {
-    const char *extension = strrchr(filename, '.'); // Extract file extension.
+    // Extract the file extension
+    const char *extension = strrchr(filename, '.');
     if (!extension) {
-        return ERR_UNKNOWN_FILE_TYPE; // Return -1 if no extension is found.
+        return ERR_UNKNOWN_FILE_TYPE; // Unknown file type
     }
 
-    // Match extension with known file types and their respective feature counts.
+    // Match the extension with the expected number of features
     if (strcmp(extension, ".E34") == 0) {
         return 16;
     } else if (strcmp(extension, ".F0") == 0) {
@@ -183,5 +160,5 @@ int getExpectedFeatureCount(const char *filename) {
         return 90;
     }
 
-    return ERR_UNKNOWN_FILE_TYPE; // Return -1 for unknown file types.
+    return ERR_UNKNOWN_FILE_TYPE; // Unknown file type
 }
--- data_reader.h
+++ data_reader.h
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <dirent.h>
 
-// Error codes
+// Error codes for various failure scenarios
 #define SUCCESS 0
 #define ERR_DIR_OPEN_FAILED 1
 #define ERR_FILE_OPEN_FAILED 2
@@ -16,45 +16,66 @@
 #define ERR_FEATURES_VALUES 6
 
 // Structure representing the data of a shape.
-// It includes the class, sample number, an array of feature values, and the count of these features.
 typedef struct {
     int class;           // Class identifier of the shape.
     int sample;          // Sample number for the shape.
-    double *features;    // Pointer to an array of feature values.
+    double *features;    // Array of feature values.
     int featureCount;    // Number of elements in the features array.
 } ShapeData;
 
-
-// Reads all files with a specified extension in a given directory.
-// Parameters:
-//   directory: Path to the directory containing files.
-//   extension: File extension to filter the files to be read.
-//   count: Pointer to an integer to store the number of files read.
-// Returns:
-//   Pointer to an array of ShapeData, each element representing one file's data.
+/**
+ * Reads all files with a specified extension in a given directory.
+ * @param directory Path to the directory containing files.
+ * @param extension File extension to filter the files to be read.
+ * @param count Pointer to an integer to store the number of files read.
+ * @return Pointer to an array of ShapeData, each element representing one file's data.
+ */
 ShapeData* readAllFiles(const char *directory, const char *extension, int *count);
 
-
-// Reads and processes a single file to extract shape data.
-// Parameters:
-//   filename: Path to the file to be read.
-// Returns:
-//   ShapeData structure containing data extracted from the file.
+/**
+ * Reads and processes a single file to extract shape data.
+ * @param filename Path to the file to be read.
+ * @return ShapeData structure containing data extracted from the file.
+ */
 ShapeData readFile(const char *filename);
 
-
-// Frees the dynamically allocated memory for an array of ShapeData.
-// Parameters:
-//   data: Pointer to the array of ShapeData.
-//   count: Number of elements in the array.
+/**
+ * Frees the dynamically allocated memory for an array of ShapeData.
+ * @param data Pointer to the array of ShapeData.
+ * @param count Number of elements in the array.
+ */
 void freeShapeData(ShapeData *data, int count);
 
-
-// Determines the expected number of features in a file based on its extension.
-// Parameters:
-//   filename: Path to the file.
-// Returns:
-//   Expected number of features for known file types, or -1 for unknown types.
+/**
+ * Determines the expected number of features in a file based on its extension.
+ * @param filename Path to the file.
+ * @return Expected number of features for known file types, or ERR_UNKNOWN_FILE_TYPE for unknown types.
+ */
 int getExpectedFeatureCount(const char *filename);
+
+/**
+ * Parses the filename to extract class and sample information.
+ * @param filename The filename to parse.
+ * @param class Pointer to store the extracted class number.
+ * @param sample Pointer to store the extracted sample number.
+ * @return SUCCESS if parsing is successful, ERR_INVALID_FILENAME otherwise.
+ */
+int parseFilename(const char *filename, int *class, int *sample);
+
+/**
+ * Allocates memory for storing feature values in ShapeData.
+ * @param data Pointer to the ShapeData structure where features should be stored.
+ * @return SUCCESS if memory allocation is successful, ERR_MEMORY_ALLOCATION_FAILED otherwise.
+ */
+int allocateFeatures(ShapeData *data);
+
+/**
+ * Reads feature values from a file and stores them in an array.
+ * @param file Pointer to the FILE object to read from.
+ * @param features Pointer to the array where features should be stored.
+ * @param featureCount The number of features to read.
+ * @return SUCCESS if all features are read successfully, ERR_FEATURES_VALUES otherwise.
+ */
+int readFeaturesFromFile(FILE *file, double *features, int featureCount);
 
 #endif // DATA_READER_H
