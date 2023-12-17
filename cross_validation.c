df41a852c309a9d0f19e4e1f19d7806de5c08eef
elnidala <latorrs.daniel1@gmail.com>
2023-12-17 19:56:53 +0100

    code correction and doxygen documentation

--- cross_validation.c
+++ cross_validation.c
@@ -1,11 +1,17 @@
 #include "cross_validation.h"
 
+// Perform k-fold cross-validation on a dataset using a specified model.
 void crossValidation(ShapeData *data, int dataSize, int kFolds, ModelFunction modelFunc) {
+    // Calculate the size of each fold
     int foldSize = dataSize / kFolds;
 
+    // Iterate through each fold
     for (int i = 0; i < kFolds; i++) {
+        // Calculate the start and end indices for the test set in this fold
         int testStart = i * foldSize;
         int testEnd = testStart + foldSize;
+
+        // Create a structure to hold the training and test sets
         SplitData split;
         split.trainingSet = malloc((dataSize - foldSize) * sizeof(ShapeData));
         split.testSet = malloc(foldSize * sizeof(ShapeData));
@@ -15,18 +21,19 @@ void crossValidation(ShapeData *data, int dataSize, int kFolds, ModelFunction mo
         // Populate training and test sets
         for (int j = 0; j < dataSize; j++) {
             if (j >= testStart && j < testEnd) {
+                // Add the data point to the test set
                 split.testSet[split.testSize++] = data[j];
             } else {
+                // Add the data point to the training set
                 split.trainingSet[split.trainingSize++] = data[j];
             }
         }
 
-        // Call the model function
+        // Call the model function to evaluate the model on the current fold
         modelFunc(split);
 
-        // Free memory
+        // Free memory allocated for the training and test sets
         free(split.trainingSet);
         free(split.testSet);
     }
 }
-
--- cross_validation.h
+++ cross_validation.h
@@ -1,12 +1,54 @@
+
+/**
+ * @file cross_validation.h
+ * @brief Header file for implementing k-fold cross-validation with model evaluation metrics.
+ */
+
 #ifndef CROSS_VALIDATION_H
 #define CROSS_VALIDATION_H
 
 #include "data_split.h"  // Includes ShapeData structure definition
+#include "confusion_matrix.h" // Includes ConfusionMatrix definition
 
-// In cross_validation.h
-typedef void (*ModelFunction)(SplitData split);
+/**
+ * @typedef ModelFunction
+ * @brief Typedef for a function pointer representing a model's evaluation function.
+ * @param split The data split containing training and test sets.
+ * @return ConfusionMatrix representing the model's performance.
+ */
+typedef ConfusionMatrix (*ModelFunction)(SplitData split);
 
+/**
+ * @struct CrossValidationMetrics
+ * @brief Structure to hold aggregated metrics from k-fold cross-validation.
+ */
+typedef struct {
+    double totalAccuracy;  /**< Total accuracy across all folds. */
+    double totalPrecision; /**< Total precision across all folds. */
+    double totalRecall;    /**< Total recall across all folds. */
+    double totalF1Score;   /**< Total F1 score across all folds. */
+    int foldsProcessed;    /**< Number of folds processed. */
+} CrossValidationMetrics;
+
+
+/**
+ * @brief Perform k-fold cross-validation on a dataset using a specified model.
+ *
+ * This function divides the dataset into k folds, trains the model on k-1 folds,
+ * and evaluates its performance on the remaining fold. This process is repeated k times
+ * to ensure each fold serves as the test set exactly once.
+ *
+ * @param data Pointer to the dataset.
+ * @param dataSize Number of elements in the dataset.
+ * @param kFolds Number of folds for cross-validation.
+ * @param modelFunc Function pointer to the model's evaluation function.
+ */
 void crossValidation(ShapeData *data, int dataSize, int kFolds, ModelFunction modelFunc);
+
+/**
+ * @brief Model function for k-Nearest Neighbors (knn) algorithm.
+ * @param split The data split containing training and test sets.
+ */
 void knnModelFunction(SplitData split);
 
 #endif // CROSS_VALIDATION_H
